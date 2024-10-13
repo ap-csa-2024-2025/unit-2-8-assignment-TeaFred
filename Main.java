12f4cd89ec19ae9ee9c53f003563409c096d0c1d
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-10-13 05:50:03 +0000

    problem 1 done

--- Main.java
+++ Main.java
@@ -4,8 +4,15 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // test your methods here
+    printRandom3(6);
   }
 
-  // write your methods here
+  public static void printRandom3(int n)
+  {
+    int refNum = n+1;
+    int num1 = (int)(Math.random() * refNum) + 2;
+    int num2 = (int)(Math.random() * refNum) + 2;
+    int num3 = (int)(Math.random() * refNum) + 2;
+    System.out.println("Random numbers: " + "\n"+ num1 + "\n" + num2 + "\n" +num3);
+  }
 }
