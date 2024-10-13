9a9ae19a3a558cc78520258262ee7e6c7a04070d
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-10-13 06:01:03 +0000

    completed homework

--- Main.java
+++ Main.java
@@ -4,7 +4,26 @@ public class Main
 {
   public static void main(String[] args)
   {
-    printRandom3(6);
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int n = scan.nextInt();
+    printRandom3(n);
+
+    System.out.println("Enter the first x-coordinate: ");
+    double x1 = scan.nextDouble();
+    System.out.println("Enter the first y-coordinate: ");
+    double y1 = scan.nextDouble();
+    System.out.println("Enter the second x-coordinate: ");
+    double x2 = scan.nextDouble();
+    System.out.println("Enter the second y-coordinate: ");
+    double y2 = scan.nextDouble();
+    calcSlope(x1,y1,x2,y2);
+
+    System.out.println("Enter two doubles: ");
+    double d1 = scan.nextDouble();
+    double d2 = scan.nextDouble();
+    roundedDist(d1,d2);
+  
   }
 
   public static void printRandom3(int n)
@@ -14,5 +33,15 @@ public class Main
     int num2 = (int)(Math.random() * refNum) + 2;
     int num3 = (int)(Math.random() * refNum) + 2;
     System.out.println("Random numbers: " + "\n"+ num1 + "\n" + num2 + "\n" +num3);
+  }
+
+  public static void calcSlope(double x1, double y1, double x2, double y2)
+  {
+    System.out.println((y2-y1)/(x2-x1));
+  }
+
+  public static void roundedDist(double d1, double d2)
+  {
+    System.out.println("Distance between d1 and d2 is: " + (int) (Math.abs(d1-d2) + 0.5));
   }
 }
